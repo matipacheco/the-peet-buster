3719d4b0b5ddbcf4a2958b04e72030f4a8f27e02
Mati <matipacheco@gmail.com>
2019-02-14 22:59:05 -0300

    Now we are talking

--- peet_buster.sh
+++ peet_buster.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-git log | grep $1 | wc -l
+git log | grep -c $1 > word_count
+
+COUNT=$(cat word_count)
+rm word_count
+
+echo "Number of contributions: ${COUNT}"
+
+if [[ $COUNT = 0 ]]; then
+	echo
+	echo "Watch out! There's a Peet over here!"
+	echo "¯\_(ツ)_/¯"
+fi
